70bf7842a0f76bac70fbd61f702e128429a1f9c6
Alison Golden <alisong779@yahoo.com>
2019-08-05 16:30:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//Your code here
+//Your code Here
+function justInvoke(fn){
+  return fn();
+}
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg);
+}
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+}
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  return functionToBeCopied.bind(thisValue);
+}
